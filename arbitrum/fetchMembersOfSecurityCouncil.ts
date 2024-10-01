76fc45fcac2dc06e0b70321937634b333d55bbe8
gloomydumber <chromacrow@naver.com>
2024-10-01 21:27:51 +0900

    add : fetch ARB SC members

---
+++ arbitrum/fetchMembersOfSecurityCouncil.ts
@@ -0,0 +1,28 @@
+import { ReadContractErrorType } from 'viem';
+import { mainnetPublicClient } from '../clients/publicClient';
+
+const abi = [
+  {
+    inputs: [],
+    name: 'getOwners',
+    outputs: [{ internalType: 'address[]', name: '', type: 'address[]' }],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
+
+export default async function fetchMembersOfSecurityCouncil() {
+  try {
+    const members = await mainnetPublicClient.readContract({
+      address: '0xF06E95eF589D9c38af242a8AAee8375f14023F85',
+      abi,
+      functionName: 'getOwners',
+    });
+
+    console.log(members);
+  } catch (e) {
+    const error = e as ReadContractErrorType;
+
+    console.log(error.name);
+  }
+}
